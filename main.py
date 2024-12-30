6d66cbb725263644eeb8faf52ab9fefc113c066c
khurram676 <khurramshabir475@gmail.com>
2024-12-29 21:44:49 -0800

    data structure py 2

---
+++ main.py
@@ -0,0 +1,5 @@
+mytuple=(1,2,3,('a','b','c'),(4,5))
+print(mytuple[3][2],mytuple[4][0])
+mytup=(1,2,3,5,6,7,7)
+for i in mytup:
+    print(i)
---
+++ mian2.py
@@ -0,0 +1,9 @@
+myset1={"orange","apple","garlic"}
+myset2={"onion","apple","grapes"}
+print(myset1.difference(myset2))
+print(myset1.intersection(myset2))
+mysets1={1,2,3,4,5}
+mysets2={1,4,5,6,4}
+print(mysets1.symmetric_difference(mysets2))
+print(mysets1.union(mysets2))
+      
